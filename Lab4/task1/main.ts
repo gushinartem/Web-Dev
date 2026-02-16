5cacf538e42bedb341c7fcddfbeebf1cc46b6004
Artem <gushinartem58@gmail.com>
2026-02-16 16:43:11 +0500

    done task1

---
+++ Lab4/task1/main.ts
@@ -0,0 +1,217 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` Hello Universe `,
+  styles: `
+    :host {
+      color: #a144eb;
+    }
+  `,
+})
+export class App {}
+
+
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` Hello {{ city }}, {{ 1 + 1 }} `,
+})
+export class App {
+  city = 'San Francisco';
+}
+
+
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    @for (user of users; track user.id) {
+      <p>{{ user.name }}</p>
+    }
+  `,
+})
+export class App {
+  users = [
+    {id: 0, name: 'Sarah'},
+    {id: 1, name: 'Amy'},
+    {id: 2, name: 'Rachel'},
+    {id: 3, name: 'Jessica'},
+    {id: 4, name: 'Poornima'},
+  ];
+}
+
+
+
+import {Component, input} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template: ` <p>The user's name is {{ name() }}</p> `,
+})
+export class User {
+  readonly name = input<string>();
+}
+
+
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>Username: {{ username }}</p>
+    <p>Preferred Framework:</p>
+    <ul>
+      <li>
+        Static Image:
+        <img src="/logo.svg" alt="Angular logo" />
+      </li>
+      <li>
+        Dynamic Image:
+        <img [src]="logoUrl" [alt]="logoAlt" />
+      </li>
+    </ul>
+  `,
+  imports: [],
+})
+export class User {
+  logoUrl = '/logo.svg';
+  logoAlt = 'Angular logo';
+  username = 'youngTech';
+}
+
+
+
+
+import {Routes} from '@angular/router';
+
+import {Home} from './home/home';
+import {User} from './user/user';
+
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'App Home Page',
+    component: Home,
+  },
+  {
+    path: 'user',
+    title: 'App User Page',
+    component: User,
+  },
+];
+
+
+
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>Username: {{ username }}</p>
+    <p>{{ username }}'s favorite framework: {{ favoriteFramework }}</p>
+    <label for="framework">
+      Favorite Framework:
+      <input id="framework" type="text" [(ngModel)]="favoriteFramework" />
+    </label>
+  `,
+  imports: [FormsModule],
+})
+export class User {
+  favoriteFramework = '';
+  username = 'youngTech';
+}
+
+
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>Username: {{ username }}</p>
+    <p>Framework: {{ favoriteFramework }}</p>
+    <label for="framework">
+      Favorite Framework:
+      <input id="framework" type="text" [(ngModel)]="favoriteFramework" />
+    </label>
+    <button (click)="showFramework()">Show Framework</button>
+  `,
+  imports: [FormsModule],
+})
+export class User {
+  favoriteFramework = '';
+  username = 'youngTech';
+
+  showFramework() {
+    alert(this.favoriteFramework);
+  }
+}
+
+
+import {Component} from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+import {ReactiveFormsModule, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <form [formGroup]="profileForm">
+      <input type="text" formControlName="name" name="name" />
+      <input type="email" formControlName="email" name="email" />
+      <button type="submit" [disabled]="!profileForm.valid">Submit</button>
+    </form>
+  `,
+  imports: [ReactiveFormsModule],
+})
+export class App {
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+}
+
+
+import {Component} from '@angular/core';
+import {DecimalPipe, DatePipe, CurrencyPipe} from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <ul>
+      <li>Number with "decimal" {{ num | number: '3.2-2' }}</li>
+      <li>Date with "date" {{ birthday | date: 'medium' }}</li>
+      <li>Currency with "currency" {{ cost | currency }}</li>
+    </ul>
+  `,
+  imports: [DecimalPipe, DatePipe, CurrencyPipe],
+})
+export class App {
+  num = 103.1234;
+  birthday = new Date(2023, 3, 2);
+  cost = 4560.34;
+}
+
+
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'reverse',
+})
+export class ReversePipe implements PipeTransform {
+  transform(value: string): string {
+    let reverse = '';
+
+    for (let i = value.length - 1; i >= 0; i--) {
+      reverse += value[i];
+    }
+
+    return reverse;
+  }
+}
